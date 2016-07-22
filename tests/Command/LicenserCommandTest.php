aebe53c859bac3f4cfbe6a5ea0f9363dc01d5d96
Rafael SR <raf.rsr@gmail.com>
2016-07-22 00:07:11 -0400

    refactoring tests

--- tests/Command/LicenserCommandTest.php
+++ tests/Command/LicenserCommandTest.php
@@ -16,7 +16,6 @@ use Rafrsr\Licenser\Tests\LicenseTesterSetUpTrait;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Tests\Fixtures\DummyOutput;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 
 class LicenserCommandTest extends \PHPUnit_Framework_TestCase
 {
@@ -39,123 +38,6 @@ class LicenserCommandTest extends \PHPUnit_Framework_TestCase
                 'source' => realpath($this->tempDir),
             ]
         );
-        $output = new DummyOutput();
-        $command->run($input, $output);
-    }
-
-    public function testLicenserWithCustomLicense()
-    {
-        $licenser = self::getMockBuilder(Licenser::class)->disableOriginalConstructor()->getMock();
-        $licenser->expects(self::once())->method('process')->with(Licenser::MODE_NORMAL);
-
-        $config = Config::create()
-            ->setLicense(file_get_contents($this->fixturesDir.DIRECTORY_SEPARATOR.'license'))
-            ->setFinder(Finder::create()->name('*.php')->in(realpath($this->tempDir)));
-
-        $command = self::getMockBuilder(LicenserCommand::class)->setMethods(['buildLicenser'])->getMock();
-        $command->expects(self::once())->method('buildLicenser')->with($config)->willReturn($licenser);
-
-        $input = new ArrayInput(
-            [
-                'source' => realpath($this->tempDir),
-                'license' => realpath($this->fixturesDir.DIRECTORY_SEPARATOR.'license'),
-            ]
-        );
-        $output = new DummyOutput();
-        $command->run($input, $output);
-    }
-
-    public function testLicenserWithParameters()
-    {
-        $licenser = self::getMockBuilder(Licenser::class)->disableOriginalConstructor()->getMock();
-        $licenser->expects(self::once())->method('process')->with(Licenser::MODE_NORMAL);
-
-        $config = Config::create()
-            ->setLicense(file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', 'src', 'licenses', 'default'])))
-            ->setFinder(Finder::create()->name('*.php')->in(realpath($this->tempDir)))
-            ->setParameters(
-                [
-                    'author' => 'AuthorName',
-                    'version' => 'v1.0',
-                ]
-            );
-
-        $command = self::getMockBuilder(LicenserCommand::class)->setMethods(['buildLicenser'])->getMock();
-        $command->expects(self::once())->method('buildLicenser')->with($config)->willReturn($licenser);
-
-        $input = new ArrayInput(
-            [
-                'source' => realpath($this->tempDir),
-                '-p' => [
-                    'author:AuthorName',
-                    'version:v1.0',
-                ],
-            ]
-        );
-        $output = new DummyOutput();
-        $command->run($input, $output);
-    }
-
-    public function testLicenserForOneFile()
-    {
-        $licenser = self::getMockBuilder(Licenser::class)->disableOriginalConstructor()->getMock();
-        $licenser->expects(self::once())->method('process')->with(Licenser::MODE_NORMAL);
-
-        $config = Config::create()
-            ->setLicense(file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', 'src', 'licenses', 'default'])))
-            ->setFinder(Finder::create()->name('file.php')->in(realpath($this->tempDir))->depth(0));
-
-        $command = self::getMockBuilder(LicenserCommand::class)->setMethods(['buildLicenser'])->getMock();
-        $command->expects(self::once())->method('buildLicenser')->with($config)->willReturn($licenser);
-
-        $input = new ArrayInput(
-            [
-                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file.php'),
-            ]
-        );
-        $output = new DummyOutput();
-        $command->run($input, $output);
-    }
-
-    public function testLicenserForInvalidFile()
-    {
-        $command = self::getMockBuilder(LicenserCommand::class)->setMethods(['buildLicenser'])->getMock();
-        $input = new ArrayInput(
-            [
-                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file_none.php'),
-            ]
-        );
-        self::expectException(FileNotFoundException::class);
-        $output = new DummyOutput();
-        $command->run($input, $output);
-    }
-
-    public function testLicenserInvalidParameter()
-    {
-        $command = self::getMockBuilder(LicenserCommand::class)->setMethods(['buildLicenser'])->getMock();
-        $input = new ArrayInput(
-            [
-                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file.php'),
-                '-p' => [
-                    'author' => 'AuthorName',
-                ],
-            ]
-        );
-        self::setExpectedExceptionRegExp(\InvalidArgumentException::class, '/Invalid parameter "AuthorName"/');
-        $output = new DummyOutput();
-        $command->run($input, $output);
-    }
-
-    public function testLicenserInvalidLicenseFile()
-    {
-        $command = self::getMockBuilder(LicenserCommand::class)->setMethods(['buildLicenser'])->getMock();
-        $input = new ArrayInput(
-            [
-                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file.php'),
-                'license' => 'my_license',
-            ]
-        );
-        self::setExpectedExceptionRegExp(\InvalidArgumentException::class, '/Invalid license file "my_license"/');
         $output = new DummyOutput();
         $command->run($input, $output);
     }
--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -9,11 +9,16 @@
 
 namespace Rafrsr\Licenser\Tests;
 
+use Rafrsr\Licenser\Command\LicenserCommand;
 use Rafrsr\Licenser\Config;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 
 class ConfigTest extends \PHPUnit_Framework_TestCase
 {
+    use LicenseTesterSetUpTrait;
+
     public function testSetGetFinder()
     {
         $finder = Finder::create();
@@ -42,5 +47,119 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
         self::assertEquals('v1.0', $config->getParameter('version'));
         self::assertTrue($config->hasParameter('version'));
         self::assertFalse($config->hasParameter('name'));
+    }
+
+    public function testCreateFromInputBasic()
+    {
+        $config = Config::create()
+            ->setLicense(file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'src', 'licenses', 'default'])))
+            ->setFinder(Finder::create()->name('*.php')->in(realpath($this->tempDir)));
+
+        $input = $this->buildInput(
+            [
+                'source' => realpath($this->tempDir),
+            ]
+        );
+
+        self::assertEquals($config, Config::createFromInput($input));
+    }
+
+    public function tesetCreateFromInputCustomLicense()
+    {
+        $config = Config::create()
+            ->setLicense(file_get_contents($this->fixturesDir.DIRECTORY_SEPARATOR.'license'))
+            ->setFinder(Finder::create()->name('*.php')->in(realpath($this->tempDir)));
+
+        $input = $this->buildInput(
+            [
+                'source' => realpath($this->tempDir),
+                'license' => realpath($this->fixturesDir.DIRECTORY_SEPARATOR.'license'),
+            ]
+        );
+        self::assertEquals($config, Config::createFromInput($input));
+    }
+
+    public function testCreateFromInputWithParameters()
+    {
+        $config = Config::create()
+            ->setLicense(file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'src', 'licenses', 'default'])))
+            ->setFinder(Finder::create()->name('*.php')->in(realpath($this->tempDir)))
+            ->setParameters(
+                [
+                    'author' => 'AuthorName',
+                    'version' => 'v1.0',
+                ]
+            );
+
+        $input = $this->buildInput(
+            [
+                'source' => realpath($this->tempDir),
+                '-p' => [
+                    'author:AuthorName',
+                    'version:v1.0',
+                ],
+            ]
+        );
+        self::assertEquals($config, Config::createFromInput($input));
+    }
+
+    public function testCreateFromInputOneFile()
+    {
+        $config = Config::create()
+            ->setLicense(file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'src', 'licenses', 'default'])))
+            ->setFinder(Finder::create()->name('file.php')->in(realpath($this->tempDir))->depth(0));
+
+        $input = $this->buildInput(
+            [
+                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file.php'),
+            ]
+        );
+        self::assertEquals($config, Config::createFromInput($input));
+    }
+
+    public function testCreateFromInputForInvalidFile()
+    {
+        $input = $this->buildInput(
+            [
+                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file_none.php'),
+            ]
+        );
+        self::expectException(FileNotFoundException::class);
+        Config::createFromInput($input);
+    }
+
+    public function testCreateFromInputInvalidParameter()
+    {
+        $input = $this->buildInput(
+            [
+                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file.php'),
+                '-p' => [
+                    'author' => 'AuthorName',
+                ],
+            ]
+        );
+        self::setExpectedExceptionRegExp(\InvalidArgumentException::class, '/Invalid parameter "AuthorName"/');
+        Config::createFromInput($input);
+    }
+
+    public function testCreateFromInputInvalidLicenseFile()
+    {
+        $input = $this->buildInput(
+            [
+                'source' => realpath($this->tempDir.DIRECTORY_SEPARATOR.'file.php'),
+                'license' => 'my_license',
+            ]
+        );
+        self::setExpectedExceptionRegExp(\InvalidArgumentException::class, '/Invalid license file "my_license"/');
+        Config::createFromInput($input);
+    }
+
+    protected function buildInput($array)
+    {
+        $input = new ArrayInput($array);
+        $command = new LicenserCommand();
+        $input->bind($command->getDefinition());
+
+        return $input;
     }
 }
