6876bb9c6633ecab8e9fb5434d3430680239a6c9
Rafael SR <raf.rsr@gmail.com>
2016-07-27 13:31:24 -0400

    update license headers

--- tests/ConfigFactoryTest.php
+++ tests/ConfigFactoryTest.php
@@ -1,10 +1,14 @@
 <?php
 
-/**
+/*
  * LICENSE: This file is subject to the terms and conditions defined in
  * file 'LICENSE', which is part of this source code package.
  *
  * @copyright 2016 Copyright(c) - All rights reserved.
+ *
+ * @author Rafael SR <https://github.com/rafrsr>
+ * @package Licenser
+ * @version 1.0.4
  */
 namespace Rafrsr\Licenser\Tests;
 
